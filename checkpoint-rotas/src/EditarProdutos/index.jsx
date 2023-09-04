9653addd71b0e56b0224ff418660850ecf76ca36
aaaaaaaaana <rm551401@fiap.com.br>
2023-09-04 15:32:19 -0300

    adicionando conteudo no componente

---
+++ checkpoint-rotas/src/EditarProdutos/index.jsx
@@ -0,0 +1,15 @@
+export default function EditarProdutos(){
+
+    return(
+
+        <main>
+
+            <h1>Editando o produto</h1>
+
+        </main>
+
+    )
+
+
+
+}
---
+++ checkpoint-rotas/src/Error/inde.jsx
@@ -0,0 +1,15 @@
+export default function Error(){
+
+    return(
+
+        <main>
+
+            <h1>erro 404 - pagina nao encontrada</h1>
+
+        </main>
+
+    )
+
+
+
+}
