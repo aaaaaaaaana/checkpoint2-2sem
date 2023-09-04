5447bf539e73d0d5f3fcfcbfff5b37bc3cd7afd2
aaaaaaaaana <rm551401@fiap.com.br>
2023-09-04 19:20:08 -0300

    configurando rotas

--- checkpoint-rotas/src/main.jsx
+++ checkpoint-rotas/src/main.jsx
@@ -9,6 +9,36 @@ import Produtos from './routes/Produtos/index.jsx'
 import Error from './routes/Error/inde.jsx'
 import EditarProdutos from './routes/EditarProdutos/index.jsx'
 
+const router = createBrowserRouter([
+
+  {
+
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+
+      {
+        path: "/produtos",
+        element: <Produtos/>
+      },
+
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProdutos/>
+      }
+    ]
+
+
+
+  }
+
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
